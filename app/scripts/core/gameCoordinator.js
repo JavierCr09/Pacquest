895e5c086945d6f40061fc87fc35c2069a7171b9
JavierCr09 <suitesfrog@gmail.com>
2026-05-10 19:44:25 -0500

    Mi 11 commit del proyecto Pacman

--- app/scripts/core/gameCoordinator.js
+++ app/scripts/core/gameCoordinator.js
@@ -127,14 +127,19 @@ class GameCoordinator {
       });
       document.addEventListener('fullscreenchange', () => {
         const icon = document.getElementById('fullscreen-icon');
-        if (!icon) return;
-        if (document.fullscreenElement) {
-          // Show exit-fullscreen icon (compress arrows)
-          icon.innerHTML = '<path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>';
-        } else {
-          // Show enter-fullscreen icon (expand arrows)
-          icon.innerHTML = '<path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>';
+        if (icon) {
+          if (document.fullscreenElement) {
+            // Show exit-fullscreen icon (compress arrows)
+            icon.innerHTML = '<path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>';
+          } else {
+            // Show enter-fullscreen icon (expand arrows)
+            icon.innerHTML = '<path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>';
+          }
         }
+        // Recalculate layout after the browser finishes adjusting the viewport
+        // Two passes: fast (100ms) for most browsers, slow (350ms) for those that animate
+        setTimeout(() => this.handleResize(), 100);
+        setTimeout(() => this.handleResize(), 350);
       });
     }
 
